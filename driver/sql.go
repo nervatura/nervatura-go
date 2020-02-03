8677a24d653ebf5c71ba1b635aac324a3aa3617a
nervatura <kappel.csaba@gmail.com>
2020-02-03 22:59:10 +0200

    v0.7.2

--- driver/sql.go
+++ driver/sql.go
@@ -32,28 +32,29 @@ type SQLDriver struct {
 	db      *sql.DB
 }
 
-const (
-	dcCasInt      = "{CAS_INT}"
-	dcCaeInt      = "{CAE_INT}"
-	dcCasFloat    = "{CAS_FLOAT}"
-	dcCaeFloat    = "{CAE_FLOAT}"
-	dcCasDate     = "{CAS_DATE}"
-	dcCaeDate     = "{CAE_DATE}"
-	dcFmsDate     = "{FMS_DATE}"
-	dcFmeDate     = "{FME_DATE}"
-	dcFmsDateTime = "{FMS_DATETIME}"
-	dcFmeDateTime = "{FME_DATETIME}"
-)
-
 func (ds *SQLDriver) decodeEngine(sqlStr string) string {
+	const (
+		dcCasInt      = "{CAS_INT}"
+		dcCaeInt      = "{CAE_INT}"
+		dcCasFloat    = "{CAS_FLOAT}"
+		dcCaeFloat    = "{CAE_FLOAT}"
+		dcCasDate     = "{CAS_DATE}"
+		dcCaeDate     = "{CAE_DATE}"
+		dcFmsDate     = "{FMS_DATE}"
+		dcFmeDate     = "{FME_DATE}"
+		dcFmsDateTime = "{FMS_DATETIME}"
+		dcFmeDateTime = "{FME_DATETIME}"
+		asDate        = " as date)"
+	)
+
 	switch ds.engine {
-	case "sqlite", "sqlite3":
+	case "sqlite", "sqlite3", "postgres":
 		sqlStr = strings.ReplaceAll(sqlStr, dcCasInt, "cast(")
 		sqlStr = strings.ReplaceAll(sqlStr, dcCaeInt, " as integer)")
 		sqlStr = strings.ReplaceAll(sqlStr, dcCasFloat, "cast(")
 		sqlStr = strings.ReplaceAll(sqlStr, dcCaeFloat, " as float8)")
 		sqlStr = strings.ReplaceAll(sqlStr, dcCasDate, "cast(")
-		sqlStr = strings.ReplaceAll(sqlStr, dcCaeDate, " as date)")
+		sqlStr = strings.ReplaceAll(sqlStr, dcCaeDate, asDate)
 		sqlStr = strings.ReplaceAll(sqlStr, dcFmsDate, "to_char(")
 		sqlStr = strings.ReplaceAll(sqlStr, dcFmeDate, ", 'YYYY-MM-DD')")
 		sqlStr = strings.ReplaceAll(sqlStr, dcFmsDateTime, "to_char(")
@@ -64,7 +65,7 @@ func (ds *SQLDriver) decodeEngine(sqlStr string) string {
 		sqlStr = strings.ReplaceAll(sqlStr, dcCasFloat, "cast(")
 		sqlStr = strings.ReplaceAll(sqlStr, dcCaeFloat, " as decimal)")
 		sqlStr = strings.ReplaceAll(sqlStr, dcCasDate, "cast(")
-		sqlStr = strings.ReplaceAll(sqlStr, dcCaeDate, " as date)")
+		sqlStr = strings.ReplaceAll(sqlStr, dcCaeDate, asDate)
 		sqlStr = strings.ReplaceAll(sqlStr, dcFmsDate, "date_format(")
 		sqlStr = strings.ReplaceAll(sqlStr, dcFmeDate, ", '%Y-%m-%d')")
 		sqlStr = strings.ReplaceAll(sqlStr, dcFmsDateTime, "date_format(")
@@ -75,22 +76,11 @@ func (ds *SQLDriver) decodeEngine(sqlStr string) string {
 		sqlStr = strings.ReplaceAll(sqlStr, dcCasFloat, "cast(")
 		sqlStr = strings.ReplaceAll(sqlStr, dcCaeFloat, " as float)")
 		sqlStr = strings.ReplaceAll(sqlStr, dcCasDate, "cast(")
-		sqlStr = strings.ReplaceAll(sqlStr, dcCaeDate, " as date)")
+		sqlStr = strings.ReplaceAll(sqlStr, dcCaeDate, asDate)
 		sqlStr = strings.ReplaceAll(sqlStr, dcFmsDate, "convert(varchar(10),")
 		sqlStr = strings.ReplaceAll(sqlStr, dcFmeDate, ", 120)")
 		sqlStr = strings.ReplaceAll(sqlStr, dcFmsDateTime, "convert(varchar(16),")
 		sqlStr = strings.ReplaceAll(sqlStr, dcFmeDateTime, ", 120)")
-	case "postgres":
-		sqlStr = strings.ReplaceAll(sqlStr, dcCasInt, "cast(")
-		sqlStr = strings.ReplaceAll(sqlStr, dcCaeInt, " as integer)")
-		sqlStr = strings.ReplaceAll(sqlStr, dcCasFloat, "cast(")
-		sqlStr = strings.ReplaceAll(sqlStr, dcCaeFloat, " as float8)")
-		sqlStr = strings.ReplaceAll(sqlStr, dcCasDate, "cast(")
-		sqlStr = strings.ReplaceAll(sqlStr, dcCaeDate, " as date)")
-		sqlStr = strings.ReplaceAll(sqlStr, dcFmsDate, "to_char(")
-		sqlStr = strings.ReplaceAll(sqlStr, dcFmeDate, ", 'YYYY-MM-DD')")
-		sqlStr = strings.ReplaceAll(sqlStr, dcFmsDateTime, "to_char(")
-		sqlStr = strings.ReplaceAll(sqlStr, dcFmeDateTime, ", 'YYYY-MM-DD HH24:MI')")
 	}
 	//print(sqlStr)
 	return sqlStr
@@ -638,19 +628,13 @@ func (ds *SQLDriver) decodeSQL(queries []ntura.Query) (string, []interface{}, er
 		if len(query.Filters) > 0 || query.Filter != "" {
 			sqlString += " where "
 		}
-		if len(query.Filters) > 0 {
-			for wi := 0; wi < len(query.Filters); wi++ {
-				sqlString, params = ds.getFilterString(query.Filters[wi], (wi == 0), sqlString, params)
-			}
+		for wi := 0; wi < len(query.Filters); wi++ {
+			sqlString, params = ds.getFilterString(query.Filters[wi], (wi == 0), sqlString, params)
 		}
-		if query.Filter != "" {
-			sqlString += query.Filter
-		}
-		if len(query.OrderBy) > 0 {
-			order := strings.Join(query.OrderBy, ",")
-			if order != "" {
-				sqlString += " order by " + order
-			}
+		sqlString += query.Filter
+		order := strings.Join(query.OrderBy, ",")
+		if order != "" {
+			sqlString += " order by " + order
 		}
 	}
 	return strings.Trim(sqlString, " "), params, nil
